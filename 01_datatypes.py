aebcccd957ea8203a852f06b8315140cac3284f8
Robert Vári <mail.robertvari@gmail.com>
2022-07-02 15:46:34 +0200

    init

--- 01_datatypes.py
+++ 01_datatypes.py
@@ -1,16 +1,20 @@
-# ez egy string: "Hello World"
-print("Hello World")
-
-# masik karaktert is hasznalhatsz a string jelolesre: ''
-print('Robert Vari')
-
-# Specialis esetek
-print("I'ts my birthday")
-print('I\'ts my birthday')
+# # ez egy string: "Hello World"
+# print("Hello World")
+#
+# # masik karaktert is hasznalhatsz a string jelolesre: ''
+# print('Robert Vari')
+#
+# # Specialis esetek
+# print("I'ts my birthday")
+# print('I\'ts my birthday')
 
 # integers: int, floats: float
 # int: 10, 100, -13, -300
-print(1977)
+# print(1977)
 
 # float: 3.14, -3.14
-print(3.14)
+# print(3.14)
+
+print("Hello" + "World")
+print(10 + 20)
+print("10" + "20")
