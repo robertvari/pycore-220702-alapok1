d7b349fc389923066138785f6e2bcc8b3acc654b
Robert Vári <mail.robertvari@gmail.com>
2022-07-02 16:39:33 +0200

    init

---
+++ 03_variables.py
@@ -0,0 +1,17 @@
+my_name = "Robert Vari"
+my_age = 42
+my_email = "mail.pythonsuli@gmail.com"
+
+# bad variable names
+# 1my_name = "hello world"
+# my name = "hello world"
+
+print(my_name)
+print(my_age)
+print(my_email)
+
+my_name = "Nagy Csilla"
+print(my_name)
+print(my_name)
+print(my_name)
+print(my_name)
