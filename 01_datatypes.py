4a6df5600756896ecd44ea9434a6f3f0bb4eb434
Robert Vári <mail.robertvari@gmail.com>
2022-07-02 15:29:22 +0200

    init

---
+++ 01_datatypes.py
@@ -0,0 +1,9 @@
+# ez egy string: "Hello World"
+print("Hello World")
+
+# masik karaktert is hasznalhatsz a string jelolesre: ''
+print('Robert Vari')
+
+# Specialis esetek
+print("I'ts my birthday")
+print('I\'ts my birthday')
