4ff70b421440b469c70f4687c33cf56fe0bc0222
Robert Vári <mail.robertvari@gmail.com>
2022-07-02 17:28:17 +0200

    init

---
+++ 05_lists.py
@@ -0,0 +1,20 @@
+my_friends = ["Robert", "Csaba", "Csilla", "Tamás", "Béla", "Róbert"]
+shopping_list = ["Tej", "Kenyér", "Szalámi"]
+
+# print(shopping_list)
+shopping_list.append("Sonka")
+# print(shopping_list)
+
+shopping_list.insert(0, "Cola")
+# print(shopping_list)
+
+# print(shopping_list[2])
+# print(shopping_list[-2])
+
+print(shopping_list)
+print(len(shopping_list))
+# print(shopping_list[5])
+
+# get the middle item in the shopping list
+print(shopping_list[ len(shopping_list) // 2 ])
+
