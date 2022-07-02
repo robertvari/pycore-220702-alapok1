7cc1b4c27ddb239da92b8054c9932c2638eb298f
Robert Vári <mail.robertvari@gmail.com>
2022-07-02 17:41:36 +0200

    init

--- 05_lists.py
+++ 05_lists.py
@@ -1,5 +1,13 @@
 my_friends = ["Robert", "Csaba", "Csilla", "Tamás", "Béla", "Róbert"]
-shopping_list = ["Tej", "Kenyér", "Szalámi"]
+shopping_list = [
+    "Tej",
+    "Kenyér",
+    "Szalámi",
+    "Sonka"
+]
+
+print(len(shopping_list) // 2)
+print(shopping_list[ len(shopping_list) // 2])
 
 # print(shopping_list)
 shopping_list.append("Sonka")
@@ -11,9 +19,18 @@ shopping_list.insert(0, "Cola")
 # print(shopping_list[2])
 # print(shopping_list[-2])
 
-print(shopping_list)
-print(len(shopping_list))
+# print(shopping_list)
+# print(len(shopping_list))
 # print(shopping_list[5])
 
 # get the middle item in the shopping list
-print(shopping_list[ len(shopping_list) // 2 ])
+# print(shopping_list[ len(shopping_list) // 2 ])
+
+# replace item in list
+shopping_list[ shopping_list.index("Szalámi") ] = "WC papír"
+# print(shopping_list)
+
+# delete item in list
+shopping_list.remove("Sonka")
+del shopping_list[shopping_list.index("Tej")]
+# print(shopping_list)
