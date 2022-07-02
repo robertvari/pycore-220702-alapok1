0e6dc0db7090242d7cc3955a660136a9eecdab2d
Robert Vári <mail.robertvari@gmail.com>
2022-07-02 17:02:51 +0200

    init

---
+++ 04_string_format.py
@@ -0,0 +1,7 @@
+user_name = input("What is your name?")
+user_email = input("What is your email?")
+
+# string format: f""
+print(f"User name: {user_name} User Email: {user_email}")
+
+# print(f"User name: " + user_name + "User Email: " + user_email)
