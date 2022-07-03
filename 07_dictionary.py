9ee3bd031ac3ba32d1a787c040e3bf8457f3be28
Robert Vári <mail.robertvari@gmail.com>
2022-07-03 14:24:12 +0200

    init

---
+++ 07_dictionary.py
@@ -0,0 +1,9 @@
+phonebook = {
+    "06 20 123 4567": {"name": "Csaba", "email": "csaba@gmail.com", "address": "Budapest"},
+    "06 20 321 7654": {"name": "Csilla", "email": "csilla@gmail.com", "address": "Pécs"},
+    "06 20 321 7623": {"name": "Tamás", "email": "tamas@gmail.com", "address": "Debrecen"},
+    "06 20 321 7345": {"name": "Gábor", "email": "gabor@gmail.com", "address": "Miskolc"},
+}
+
+print(phonebook["06 20 123 4567"])
+print(phonebook["06 20 123 4567"]["address"])
