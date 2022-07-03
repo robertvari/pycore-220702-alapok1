70d04bc575d8dbac8f6a57b45a60b430f4c7d9a4
Robert Vári <mail.robertvari@gmail.com>
2022-07-03 17:02:49 +0200

    update

---
+++ 15_while_loop.py
@@ -0,0 +1,13 @@
+import time
+
+number = 0
+
+#        True
+while number < 10:
+    print(number)
+    number += 1
+
+
+# while True:
+#     print("check this website: www.google.com")
+#     time.sleep(1)
